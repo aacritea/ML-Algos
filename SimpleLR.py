ef733cc57e38b4bf796358671166521b02adcbe9
Aakriti Jain <163650998+aacritea@users.noreply.github.com>
2024-08-03 04:58:53 +0530

    Update SimpleLR.py

--- SimpleLR.py
+++ SimpleLR.py
@@ -1,1 +1,19 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
 
+df = pd.read_csv('placement_SLR.csv')
+
+df.head()
+
+plt.scatter(df['cgpa'],df['package'])
+plt.xlabel('CGPA')
+plt.ylabel('Package(in lpa)')
+
+X = df.iloc[:,0:1]
+y = df.iloc[:,-1]
+
+y
+
+from sklearn.model_selection import train_test_split
+X_train,X_test,y_train,y_test = train_test_split(X,y,test_size=0.2,random_state=2)
