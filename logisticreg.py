56aa4b32ebdc905c9250441858e0e32a428a84da
aacritea <aakritijain1412@gmail.com>
2024-08-03 05:53:08 +0530

    Add comments

--- logisticreg.py
+++ logisticreg.py
@@ -29,11 +29,13 @@ y=df['test_result']
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 
+# train-test split
 X_train,X_test,y_train,y_test=train_test_split(X,y, test_size=0.2, random_state=1)
 scaler=StandardScaler()
 scaled_X_train=scaler.fit_transform(X_train)
 scaled_X_test=scaler.transform(X_test)
 
+# performs classification
 from sklearn.linear_model import LogisticRegression
 log_model=LogisticRegression()
 log_model.fit(scaled_X_train, y_train)
@@ -45,5 +47,6 @@ y_pred
 y_pred_prob=log_model.predict_proba(scaled_X_test)
 y_pred_prob
 
+# checks accuracy
 accuracy_score(y_test,y_pred)
 confusion_matrix(y_test,y_pred)
